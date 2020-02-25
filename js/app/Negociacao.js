8798afb8f91c8ad5f447be04c0ddca085955a009
william <william.frazao@original.io>
2020-02-25 13:06:49 -0300

    especificando uma negociacao

--- js/app/Negociacao.js
+++ js/app/Negociacao.js
@@ -2,12 +2,91 @@ class Negociacao {
 
     constructor(data, quantidade, valor) {
 
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = new Date (data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
     }
 
-    obtemVolume() {
-        return this.quantidade * this.valor;
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+
+    get data() {
+        return new Date (this._data.getTime());
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
     }
 }
+
+class Calopsita {
+    
+    constructor(nome, raca) {
+
+        this.nome = nome;
+        this.raca = raca;
+    }
+
+}
+
+class Pessoa {
+
+    constructor(nome, sobrenome) {
+
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    obtemNomeCompleto() {
+        return this.nome + ' ' + this.sobrenome;
+    }
+
+}
+
+class Conta {   
+
+    constructor (titular, conta) {
+        this._titular = titular;
+        this._conta = conta;
+        this._saldo = 0.0;
+    }
+
+    deposita(valor) {
+        console.log('valor depositado: ' + valor);
+        this._saldo+=valor;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    get titular() {
+        return this._titular;
+    }
+
+    get conta() {
+        return this._conta;
+    }
+
+}
+
+var conta = new Conta('Mingau', 171);
+conta.deposita(100);
+console.log(conta.titular);
+console.log(conta.conta);
+console.log(conta.saldo);
+
+let idade = 18;
+let temCarteira = true;
+
+if (idade >= 18 && temCarteira) {
+    let msg = 'Pode dirigir';
+    console.log(msg)
+}
+alert(msg);
