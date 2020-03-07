9ba67cb5fe82d5478275b37cf009a912b437a55a
william <william.frazao@original.io>
2020-03-07 14:28:23 -0300

    exibindo o objeto date no formato correto, isolando a responsabilidade de conversao de datas

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -9,21 +9,20 @@ class NegociacaoController {
     }
     
     adiciona(event) {
-        
+
         event.preventDefault();
-      
-        let data = new Date(
-            ...this._inputData.value
-                .split('-')
-                .map((item, indice) => item - indice % 2)
-        );
-            
+
+        let helper = new DateHelper();
+
         let negociacao = new Negociacao(
-            data,
-            this._inputQuantidade.value,
-            this._inputValor.value  
-        );
-        
+                helper.textoParaData(this._inputData.value),
+                this._inputQuantidade.value,
+                this._inputValor.value
+            );
+
+
         console.log(negociacao);
+
+        console.log(helper.dataParaTexto(negociacao.data));
     }
 }
