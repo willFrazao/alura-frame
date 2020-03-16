35171c447346f3ca0a361ae14f08a2e4f9eab22d
william <william.frazao@original.io>
2020-03-16 08:32:59 -0300

    criando listModel e blindando as negociacoes

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -6,19 +6,30 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
     
     adiciona(event) {
 
         event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+    }
 
-        let negociacao = new Negociacao(
-                DateHelper.textoParaData(this._inputData.value),
-                this._inputQuantidade.value,
-                this._inputValor.value
-            );
+    _criaNegociacao() {
 
-        console.log(negociacao);
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+        return new Negociacao (
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+
+        this._inputData.focus();
     }
 }
