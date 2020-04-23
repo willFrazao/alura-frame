47354901c8e8311bfc685e2a3a53e9e2a121cb7b
William <William.frazao91@gmail.com>
2020-04-23 11:56:15 -0300

    nosso proxy ainda nao esta 100%

--- js/app/services/ProxyFactory.js
+++ js/app/services/ProxyFactory.js
@@ -2,11 +2,11 @@ class ProxyFactory {
 
     static create(objeto, props, acao) {
 
-        return new Proxy(new ListaNegociacoes(), {
+        return new Proxy(objeto, {
 
             get (target, prop, receiver) { 
 
-                if (props.includes(prop) && typeof(target[prop]) == typeof(Function)) {
+                if (props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
 
                     return function() {
 
@@ -17,7 +17,20 @@ class ProxyFactory {
                 }
 
                 return Reflect.get(target,prop,receiver);
+            },
+
+            set(target, prop, value, receiver) {
+                if(props.includes(prop)) {
+                    acao(target);
+                }
+
+                return Reflect.set(target, prop, value, receiver);
             }
         });
+    }
+
+    static _ehFuncao(func) {
+
+        return typeof(func) == typeof(Function);
     }
 }
